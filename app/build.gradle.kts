ec1c37a6ff33a1d5fe3420f87bc2dbe37b1f31f1
NickelSulphide101 <57768642+NickelSulphide101@users.noreply.github.com>
2026-03-22 18:55:35 +0800

    chore: Downgrade `androidx.core:core-ktx` dependency from 1.15.0 to 1.13.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation(platform("androidx.compose:compose-bom:2024.11.00"))
