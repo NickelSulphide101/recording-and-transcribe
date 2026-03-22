750160a66b249c9bf710e2723ceb4e683027f55f
NickelSulphide101 <57768642+NickelSulphide101@users.noreply.github.com>
2026-03-22 22:25:55 +0800

    feat: update Kotlin Android, Compose, Serialization, and Parcelize plugin versions to 2.2.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.2.20" apply false
 }
